2fc37195dc6fe7c25b38ece2fe59ed4bb337b776
Denis Gutsulak <d.99113@gmail.com>
2022-07-07 16:06:29 +0300

    root

--- client/src/components/pages/MainPages.jsx
+++ client/src/components/pages/MainPages.jsx
@@ -5,9 +5,17 @@ function MainPages() {
   return (
     <Routes>
       <Route path="/" element={<p>sa</p>} />
-      <Route path="/resumes" element={<p>sa</p>} />
-      <Route path="/" element={<p>sa</p>} />
-      <Route path="/fav" element={<p>sa</p>} />
+      <Route path="/users/sign_in" element={<p>/users/sign_in</p>} />
+      <Route path="/users/sign_up" element={<p>/users/sign_up</p>} />
+      <Route path="/users" element={<p>users</p>} />
+      <Route path="/users/:id" element={<p>users/:id</p>} />
+      <Route path="/resumes" element={<p>resumes</p>} />
+      <Route path="/resumes/:id" element={<p>resumes/:id</p>} />
+      <Route path="/account/resumes/new" element={<p>account/resumes/new</p>} />
+      <Route path="/account/vacancies/new" element={<p>account/vacancies/new</p>} />
+      <Route path="/account/resumes" element={<p>account/resumes</p>} />
+      <Route path="/account/vacancies" element={<p>account/vacancies</p>} />
+      <Route path="/account/profile/edit" element={<p>account/profile/edit</p>} />
     </Routes>
   );
 }
