696ed7d1491a93aed7ffbe3cba119334d2ab5d81
Denis Gutsulak <d.99113@gmail.com>
2022-07-07 19:12:29 +0300

    vacancies Map

--- client/src/components/pages/Vacancies.jsx
+++ client/src/components/pages/Vacancies.jsx
@@ -5,6 +5,14 @@ import InputLevel from '../UI/InputLevel/InputLevel';
 function Vacancies() {
   const [langProg, setLangProg] = useState('');
   const [levelVacancies, setLevelVacancies] = useState('');
+  const [adVacancies, setAdVacancies] = useState([{
+    level: 'Джуниор',
+    leng: 'Ruby-разработчик',
+    salary: 'от 40 000 до 60 000 ₽ (На руки)',
+    company: ' Edstein',
+    body: 'Написание кода в соответствии с поставленными задачами Написание тестов Участие в отладке внутренних процессов разработки (code review, code style, git flow,deployment flow)',
+    dialect: 'PostgreSQL, ООП',
+  }]);
 
   return (
     <>
@@ -17,6 +25,35 @@ function Vacancies() {
         <input placeholder="Город" type="text" name="web_vacancies_search_form[city]" id="web_vacancies_search_form_city" />
         <input type="submit" name="commit" value="Найти" data-disable-with="Найти" />
       </div>
+      {/* kard */}
+      { adVacancies.map((el) => (
+
+        <div>
+          <div className="card mb-3 border-0">
+            <div className="card-body">
+              <div className="small text-muted">около 5 часов</div>
+              <h5 className="card-title">
+                <a href="/vacancies/576">
+                  <span className="me-2">{el.level}</span>
+                  <span className="me-2">{el.leng}</span>
+                  <span className="small me-2">{el.salary}</span>
+                  <span className="me-2">
+                    <span className="me-2">-</span>
+                    {el.company}
+                    {' '}
+                  </span>
+                </a>
+              </h5>
+              <div className="card-text mb-2">
+                {el.body}
+
+                <div className="fw-bold mt-1">{el.dialect}</div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      ))}
 
     </>
   );
