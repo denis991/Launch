0c1209ca6ff34cd0e2c7a0ad25afda8fa7eab932
Denis Gutsulak <d.99113@gmail.com>
2022-07-08 12:40:59 +0300

    action

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-// import Navbar from './components/Navbar/Navbar';
-
 import MainPages from './components/pages/MainPages';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
       <MainPages />
     </div>
   );
