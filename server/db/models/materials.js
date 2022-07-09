7691580bf7372bca71eef2770d7847e398cf499d
Nikita <ookami.eq@gmail.com>
2022-07-09 13:42:31 +0300

    Materials-Skills db

--- server/db/models/materials.js
+++ server/db/models/materials.js
@@ -9,13 +9,15 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Vacancies }) {
+    static associate({ Vacancies, Skills }) {
       this.belongsTo(Vacancies, { foreignKey: 'vacancy_id' })
+      this.belongsTo(Skills, { foreignKey: 'skill_id' })
     }
   }
   Materials.init({
     material: DataTypes.STRING,
-    vacancy_id: DataTypes.INTEGER
+    vacancy_id: DataTypes.INTEGER,
+    skill_id: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Materials',
