11f38755bd2f11a63f481e0b2e24606fed48d9fc
Denis Gutsulak <d.99113@gmail.com>
2022-07-11 12:18:04 +0300

    vacansies map edit 0.2.1

--- client/src/components/UI/InputLevel/InputLevel.jsx
+++ client/src/components/UI/InputLevel/InputLevel.jsx
@@ -4,7 +4,7 @@ import './inputLevel.css';
 function InputLevel({ levelVacancies, setLevelVacancies }) {
   return (
     <label htmlFor="inputLevel" className="xz">
-      <select className="select" id="inputLevel" value={levelVacancies} onChange={(e) => setLevelVacancies(e.target.value)} name="we">
+      <select className="form-control select optional" name="web_vacancies_search_form[level]" id="web_vacancies_search_form_level" value={levelVacancies} onChange={(e) => setLevelVacancies(e.target.value)}>
         <option value="intern">Стажёр</option>
         <option value="junior">Джуниор</option>
         <option value="middle">Мидл</option>
--- client/src/redux/actions/vacancyActions.js
+++ client/src/redux/actions/vacancyActions.js
@@ -1,4 +1,4 @@
-import { ADD_VACANCY, GET_VACANCIES } from '../types/vacancyTypes';
+import { ADD_VACANCY, GET_VACANCIES, GET_VACANCIES_ALL } from '../types/vacancyTypes';
 
 export const addVacancy = () => ({ type: ADD_VACANCY });
 
@@ -19,11 +19,25 @@ export const addVacancyThunk = (data) => async (dispatch) => {
   }
 };
 export const getVacancies = (data) => ({ type: 'GET_VACANCIES', data });
+
 export const getVacanciesThunk = () => async (dispatch) => {
   const response = await fetch('/account/vacancies');
-
   if (response.ok) {
     const data = await response.json();
     dispatch(getVacancies(data));
+  }
+};
+
+export const getVacanciesAll = (data) => ({
+  type: GET_VACANCIES_ALL,
+  payload: data,
+});
+
+export const getVacanciesAllThunk = () => async (dispatch) => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/`);
+  if (response.ok) {
+    const data = await response.json();
+    // console.log(data, 'data<_____');
+    dispatch(getVacanciesAll(data));
   }
 };
