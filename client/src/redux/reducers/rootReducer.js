97b6839be486b7695a507804ead635516c66f855
Vladislav <v.rudnitsky29@gmail.com>
2022-07-14 09:52:01 +0300

    News-Release-2.2

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -5,18 +5,12 @@ import skillsReducer from './skillReducer';
 import userVacancyReducer from './userVacancyReducer';
 import authReducer from './authReducer';
 import cvCommentReducer from './cvCommentReducer';
-<<<<<<< HEAD
-// import newsReducer from './newsReduser';
-import usersReducer from './usersReducer';
-import userReducer from './userReducer';
-=======
 import usersReducer from './usersReducer';
 import userReducer from './userReducer';
 import cvUserReducer from './cvUserReducer';
 import vacanciesReducer from './vacanciesReducer';
 import accountReducer from './accountReducer';
 // import newsReducer from './newsReduser';
->>>>>>> origin/master
 
 const rootReducer = combineReducers({
   user: authReducer,
@@ -27,16 +21,11 @@ const rootReducer = combineReducers({
   skills: skillsReducer,
   cvs: cvsReducer,
   cvComments: cvCommentReducer,
-<<<<<<< HEAD
   // news: newsReducer,
-  users: usersReducer,
-  statsUser: userReducer,
-=======
   vacancy: userVacancyReducer,
   vacancies: vacanciesReducer,
   accountInfoUser: accountReducer,
   // news: newsReducer,
->>>>>>> origin/master
 });
 
 export default rootReducer;
