4ca0ad0db18959fb0c8649b888742b028e3c8280
Denis Gutsulak <d.99113@gmail.com>
2022-07-15 16:29:49 +0300

    g

--- client/src/components/Navbar/TestNavbar.js
+++ client/src/components/Navbar/TestNavbar.js
@@ -15,7 +15,7 @@ function TestNavbar() {
   return (
     <div className="header">
       <div className="logoRocket">
-        <img src="https://i.pinimg.com/originals/9c/32/01/9c3201db56c2af73499b3f0c6e8c0cf4.gif" width="125px" alt="logo" />
+        <img src="https://cliply.co/wp-content/uploads/2019/09/371909290_ROCKET_400px.gif" width="125px" height="110px" alt="logo" />
       </div>
       <UserMenu />
       {user ? renderUserMenu() : <Link className="reg" to="/auth">Войти</Link>}
