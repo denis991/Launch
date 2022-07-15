3e16208a17a47cc23c34f02f2603a9cf5de8eee3
Denis Gutsulak <d.99113@gmail.com>
2022-07-15 08:34:57 +0300

    ser

--- db/database.js
+++ db/database.js
@@ -7,7 +7,6 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: process.env.DB_PORT_F,
   },
   test: {
     username: 'root',
