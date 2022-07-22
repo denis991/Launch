b41d2726b1b2278e7d30ea4123ed3ce650bb8354
Denis Gutsulak <d.99113@gmail.com>
2022-07-22 12:05:08 +0300

    delete oject

--- middlewares/checkSession.js
+++
@@ -1,9 +0,0 @@
-const checkSession = (req, res, next) => {
-  if (req.session.userId) {
-    res.locals.userId = req.session.userId;
-    return next();
-  }
-  next();
-};
-
-module.exports = checkSession;
